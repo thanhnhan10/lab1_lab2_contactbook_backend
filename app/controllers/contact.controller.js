34283670fe4ba322ea9648d96f27ba78a08832b8
thanhnhan10 <nhanb1900295@student.ctu.edu.vn>
2022-04-06 16:43:35 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,184 @@
-exports.create = (req, res) => {
-    res.send({ message: "create handler" });
+// exports.create = (req, res) => {
+//     res.send({ message: "create handler" });
+// };
+
+// exports.findAll = (req, res) => {
+//     res.send({ message: "findAll handler" });
+// };
+
+// exports.findOne = (req, res) => {
+//     res.send({ message: "findOne handler" });
+// };
+
+// exports.update = (req, res) => {
+//     res.send({ message: "update handler" });
+// };
+
+// exports.delete = (req, res) => {
+//     res.send({ message: "delete handler" });
+// };
+
+// exports.deleteAll = (req, res) => {
+//     res.send({ message: "deleteAll handler" });
+// };
+
+// exports.findAllFavorite = (req, res) => {
+//     res.send({ message: "findAllFavorite handler" });
+// };
+
+const mongoose = require('mongoose');
+const { BadRequestError } = require('../errors');
+const Contact = require("../models/contact.model");
+
+exports.create = async(req, res, next) => {
+
+    if (!req.body.name) {
+        return next(new BadRequestError(404, "Name can not be empty"));
+    }
+
+    const contact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        phone: req.body.phone,
+        favorite: req.body.favorite === true,
+    });
+
+    try {
+        const document = await contact.save();
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new BadRequestError(
+                500, "An error occurred while creating the contact"
+            )
+        );
+    }
 };
 
-exports.findAll = (req, res) => {
-    res.send({ message: "findAll handler" });
+exports.findAll = async(req, res, next) => {
+
+    const condition = {};
+    const { name } = req.query;
+    if (name) {
+        condition.name = { $regex: new RegExp(name), $option: "i" };
+    }
+
+    try {
+        const document = await Contact.find(condition);
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new BadRequestError(
+                500, "An error occurred while retrieving contact"
+            )
+        );
+    }
 };
 
-exports.findOne = (req, res) => {
-    res.send({ message: "findOne handler" });
+
+exports.findOne = async(req, res, next) => {
+
+    const { id } = req.params;
+    const condition = {
+        _id: id && mongoose.isValidObjectId(id) ? id : null,
+    };
+
+    try {
+        const document = await Contact.findOne(condition);
+        if (!document) {
+            return next(new BadRequestError(404, "Contact not found"));
+        }
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new BadRequestError(
+                500, `Error retrieving contact with id = ${req.params.id}`
+            )
+        );
+    }
 };
 
-exports.update = (req, res) => {
-    res.send({ message: "update handler" });
+exports.update = async(req, res, next) => {
+
+    if (Object.keys(req.body).length === 0) {
+        return next(
+            new BadRequestError(400, "Data to update can not be empty"));
+    }
+
+    const { id } = req.params;
+    const condition = {
+        _id: id && mongoose.isValidObjectId(id) ? id : null,
+    };
+
+    try {
+        const document = await Contact.findOneAndUpdate(condition, req.body, {
+            new: true,
+        });
+        if (!document) {
+            return next(new BadRequestError(404, "Contact not found"));
+        }
+        return res.send({ message: "Contact was updated successfully" });
+    } catch (error) {
+        return next(
+            new BadRequestError(
+                500, `Error updating contact with id=${req.params.id}`
+            )
+        );
+    }
 };
 
-exports.delete = (req, res) => {
-    res.send({ message: "delete handler" });
+
+exports.delete = async(req, res, next) => {
+
+    const { id } = req.params;
+    const condition = {
+        _id: id && mongoose.isValidObjectId(id) ? id : null,
+    };
+
+
+    try {
+        const document = await Contact.findOneAndDelete(condition);
+        if (!document) {
+            return next(new BadRequestError(404, "Contact not found"));
+        }
+        return res.send({ message: "Contact was deleted successfully", });
+    } catch (error) {
+        return next(
+            new BadRequestError(
+                500, `Could not delete contact with id=${req.params.id}`
+            )
+        );
+    }
 };
 
-exports.deleteAll = (req, res) => {
-    res.send({ message: "deleteAll handler" });
+
+exports.findAllFavorite = async(req, res, next) => {
+
+    try {
+        const documents = await Contact.find({ favorite: true });
+        return res.send(documents);
+    } catch (error) {
+        return next(
+            new BadRequestError(
+                500, "An error occurred while retrieving favorite contacts"
+            )
+        );
+    }
 };
 
-exports.findAllFavorite = (req, res) => {
-    res.send({ message: "findAllFavorite handler" });
+exports.deleteAll = async(req, res, next) => {
+
+    try {
+        const data = await Contact.deleteMany({});
+        return res.send({
+            message: `${data.deletedCount} contacts was deleted successfully`
+        });
+    } catch (error) {
+        return next(
+            new BadRequestError(
+                500, `An error occurred while removing all contacts`
+            )
+        );
+    }
 };
---
+++ app/models/contact.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "Contact name is required"],
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+    },
+    address: String,
+    phone: String,
+    favorite: Boolean,
+}, { timestamp: true, });
+
+// Replace _id with id and remove __V
+schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
+module.exports = mongoose.model("contact", schema);
